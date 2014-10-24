42dcf19e09fb5989b6d6842fb974f3592e99dc3f
Combo Nourdine <combo.nourdine@gmail.com>
2014-10-24 10:50:26 +0200

    Ajout teiheaderElements.pl

---
+++ teiheaderElements.pl
@@ -0,0 +1,18 @@
+﻿use warnings ;
+use strict ;
+
+open (my $fichier , "< OE_31.xml") or die "impossible d'ouvrir" ;
+open (my $sortie, "> OE_31.0.xls") ;
+while (my $ligne = <$fichier>) {
+
+	if ($ligne =~ m/(<[^>]+>)/) {
+		print $sortie "$1 \n" ;
+		last if($ligne =~ /<\/teiHeader>/);
+		# if ($ligne =~ /<\/teiHeader>/) { Ceci est une aute méthode d'utiliser la fonction 'last'
+			# last;
+		#}
+	}
+}
+
+close ($sortie) ;
+close ($fichier) ;
